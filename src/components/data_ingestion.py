27fe4806956b37baf37afe4e40f5dcf3d034a14c
Sahil Chandel <sahilchandel.anee@gmail.com>
2024-10-20 12:52:29 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -27,7 +27,7 @@ class DataIngestion:
         logging.info("Entered the data ingestion method or component")
         try:
             # Use os.path.join for OS-independent paths
-            df = pd.read_csv(os.path.join('notebook', 'data', 'stud.csv'))
+            df = pd.read_csv('notebook\data\stud.csv')
             logging.info('Read the dataset as dataframe')
 
             # Create directory if it doesn't exist
--- src/utils.py
+++ src/utils.py
@@ -17,7 +17,7 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
-            pickle.dump(obj, file_obj)
+            dill.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
@@ -28,7 +28,7 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            para=param[list(models.keys())[i]]
+            para = param[list(models.keys())[i]]
 
             gs = GridSearchCV(model,para,cv=3)
             gs.fit(X_train,y_train)
@@ -36,7 +36,7 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
-            #model.fit(X_train, y_train)  # Train model
+            model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
 
@@ -53,10 +53,10 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     except Exception as e:
         raise CustomException(e, sys)
     
-# def load_object(file_path):
-#     try:
-#         with open(file_path, "rb") as file_obj:
-#             return pickle.load(file_obj)
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
-#     except Exception as e:
-#         raise CustomException(e, sys)
+    except Exception as e:
+        raise CustomException(e, sys)
